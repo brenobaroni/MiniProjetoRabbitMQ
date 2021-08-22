a537515e6c02e55374268891dc8a02f921f54f3a
Breno Baroni <breno_baroni@hotmail.com>
2021-08-22 18:03:09 -0300

    Publicador de mensagem ok

--- MiniProjetoRabbitMQ/Controllers/MessagesController.cs
+++ MiniProjetoRabbitMQ/Controllers/MessagesController.cs
@@ -1,8 +1,12 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Data.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace MiniProjetoRabbitMQ.Controllers
@@ -11,5 +15,49 @@ namespace MiniProjetoRabbitMQ.Controllers
     [ApiController]
     public class MessagesController : ControllerBase
     {
+
+        private readonly ConnectionFactory _connectionFactory;
+        private const string QUEUE_NAME = "messages";
+        //private readonly RabbitMqConfiguration _rabbitMqConfiguration;
+
+        public MessagesController()
+        {
+            _connectionFactory = new ConnectionFactory() { HostName = "localhost" };
+        }
+
+        [HttpPost]
+        public IActionResult PostMessage([FromBody] MessageInputModel message)
+        {
+            //Main AMQP Model Connection 'Advanced Message Queuing Protocol'
+            using (IConnection connection = _connectionFactory.CreateConnection())
+            {
+                //AMQP Model
+                using (IModel channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(
+                        queue: QUEUE_NAME,
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null);
+
+                    var stringfiedMessage = JsonConvert.SerializeObject(message);
+                    //RabbitMQ needs the message to be in byte array
+                    var bytesMessage = Encoding.UTF8.GetBytes(stringfiedMessage);
+
+                    channel.BasicPublish(
+                        exchange: "",
+                        routingKey: QUEUE_NAME,
+                        basicProperties: null,
+                        body: bytesMessage);
+
+
+                }
+            }
+
+            return Accepted();
+        }
+
+
     }
 }
---
+++ data/Models/MessageInputModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    public class MessageInputModel
+    {
+        public int FromId { get; set; }
+        public int ToId { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
